ff3268971fdcd675a260d82cf1fa8aecc6583106
Thatchapon Unprasert <thatchapon.unp@outlook.com>
2024-10-30 20:54:38 +0700

    Create YTMNowPlayingViewController.h

---
+++ YTMNowPlayingViewController.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+#import <YouTubeHeader/YTILikeButtonRenderer.h>
+
+@interface YTMNowPlayingViewController : UIViewController
+@property (nonatomic, strong, readwrite) YTILikeButtonRenderer *likeButtonRenderer;
+@end
